2a1fffea5d04b3a8d7ca6f2293da88e7d8567ef8
Bo Liu <wy90021@gmail.com>
2013-09-14 00:18:14 -0700

    connect to new redis and socke io

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 #$redis = Redis.new(:host => 'localhost', :port=>9999)
-$redis = Redis.new()
+#
+
+uri = URI.parse("redis://redistogo:2fe0882bcea756dec5affd1bc80564ab@crestfish.redistogo.com:10009/")
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
